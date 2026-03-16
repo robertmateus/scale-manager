c01ef572bd155c19ad8dc17cfce1174adce8da72
Robert Mateus Moreira Gomes <robert.gomes.it@gmail.com>
2026-03-16 16:04:50 +0000

    feat: configure integration tests and adjust status endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,6 +2,12 @@ import database from "infra/database.js";
 
 test("GET api/v1/status return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(Number.isNaN(Date.parse(responseBody.update_at))).toBe(false);
+
+  expect(responseBody.database.version).toBe("16.0");
+  expect(responseBody.database.max_connections).toBe(100);
+  expect(responseBody.database.opened_connections).toBeGreaterThanOrEqual(1);
 });
